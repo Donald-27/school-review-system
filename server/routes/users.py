b0ad473211adf6e780a12aeb380b34115deb3805
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-21 08:20:33 +0300

    initial users.py set up imports

---
+++ server/routes/users.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
+
+users_bp = Blueprint('users_bp', __name__, url_prefix='/users')
+
+# Create a user (Sign up)
+@users_bp.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    phone = data.get('phone')
+    password = data.get('password')
+
+    if not name or not email or not password:
+        return jsonify({"error": "Name, email, and password are required"}), 400
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error": "Email already exists"}), 409
+
+    hashed_pw = generate_password_hash(password)
+    user = User(name=name, email=email, phone=phone, password=hashed_pw)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "id": user.id,
+        "name": user.name,
+        "email": user.email
+    }), 201
+
+# User login
+@users_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user and check_password_hash(user.password, password):
+        return jsonify({
+            "message": "Login successful",
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email
+            }
+        }), 200
+    return jsonify({"error": "Invalid email or password"}), 401
+
+# Get all users (Admin access simulation)
+@users_bp.route('/', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{
+        "id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "phone": user.phone
+    } for user in users]), 200
