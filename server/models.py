489d976b6248c0fefb2c8368ba89c8def6dbf39b
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-21 08:12:25 +0300

    Added user model with name, email, phone, and password

--- server/models.py
+++ server/models.py
@@ -14,3 +14,13 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
  
+ class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(15))
+    password = db.Column(db.String(128), nullable=False)
+
+    reviews = db.relationship('Review', back_populates='user', cascade='all, delete')
