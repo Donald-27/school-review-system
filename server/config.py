d0561955e2c638ced14497b9325dc37470393ff9
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-21 08:09:49 +0300

    secret key support for Flask sessions and production config subclass for deployment

--- server/config.py
+++ server/config.py
@@ -1,7 +1,13 @@
-
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", f"sqlite:///{os.path.join(basedir, 'app.db')}")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSONIFY_PRETTYPRINT_REGULAR = True
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
